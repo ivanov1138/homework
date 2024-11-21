91b7b9bd8b379e93bc0ce59f4864101100edf8be
ivanov1138 <ivanov1138@yandex.ru>
2024-11-21 17:47:05 +0300

    Add files via upload

---
+++ wake-bake/js/main.js
@@ -0,0 +1,30 @@
+const close = document.querySelector('.burger-icon');
+const body = document.querySelector('.body');
+const bodyOpenedMenu = document.querySelector('.body--opened-menu');
+const windowWidth = window.innerWidth;
+let navLink = document.querySelector('.nav__link');
+
+if (window.innerWidth <= windowWidth) { /* Проверяю ширину экрана */
+    body.classList.add('body--opened-menu');
+}
+
+close.addEventListener('click', () => {
+    if (body.classList.contains('body--opened-menu')) {
+        body.classList.remove('body--opened-menu');
+    } else {
+        body.classList.add('body--opened-menu');
+    }
+})
+
+navLink.addEventListener('click', () => {
+    if (navLink.textContent = "Программа обучения") {
+        /* Чтото делаем */
+    } else if (navLink.textContent = "Галерея") {
+        /* Чтото делаем */
+    } else if (navLink.textContent = "Отзывы") {
+        /* Чтото делаем */
+    } else if (navLink.textContent = "Контакты") {
+        /* Чтото делаем */
+    }
+})
+
