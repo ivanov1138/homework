61c91a48edeb58191beaa4700abd9454f5546ee9
ivanov1138 <ivanov1138@yandex.ru>
2025-03-06 17:11:15 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -213,4 +213,12 @@
 
     });
 
+
+    /* ============ Маска для телефона =========== */
+
+    const telInputs = document.querySelectorAll('input[type="tel"]');
+    
+    const im = new Inputmask('+7 (999) 999-99-99');
+    im.mask(telInputs);
+
 })()
